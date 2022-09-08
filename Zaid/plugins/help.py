f8b43bf08d91038ada65a50bc9468abf9177fcfa
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-09-09 00:42:57 +0300

    Update help.py

--- Zaid/plugins/help.py
+++ Zaid/plugins/help.py
@@ -4,9 +4,13 @@ from Config import Config
 
 
 btn =[
-    [Button.inline("بەڕێوبەر", data="admin"), Button.inline("Play", data="play")],
+    [Button.inline("بەڕێوەبەر", data="admin"),],
+    [Button.inline("هەڵواسین", data="pins"), Button.inline("پاکردنەوە", data="purges")],
+    [Button.inline("کارپێ کردن", data="play"), Button.inline("لابراوەکان", data="zombies")],
+    [Button.inline("داخستن", data="locks"), Button.inline("زیاتر", data="misc")],
     [Button.inline("سەرەکی", data="start")]]
-HELP_TEXT = "بەخێربێن بۆ لیستی فەرمانەکانی Telethon سەرچاوە\n\nکلیك لە دوگمەکانی خوارەوە بکە:"
+
+HELP_TEXT = "بەخێربێن بۆ لیستی فەرمانەکانی @IQ7amo سەرچاوە\n\nکلیك لە دوگمەکانی خوارەوە بکە:"
 
 
 @Zaid.on(events.NewMessage(pattern="[!?/]help"))
@@ -14,8 +18,8 @@ async def help(event):
     if Config.MANAGEMENT_MODE == "ENABLE":
         return
     if event.is_group:
-       await event.reply("Contact me in PM to get available help menu!", buttons=[
-       [Button.url("Help And Commands!", "t.me/{}?start=help".format(BOT_USERNAME))]])
+       await event.reply("بۆ بینینی فەرمانەکان کلیك لە خواروە بکە!", buttons=[
+       [Button.url("فەرمانەکان و یارمەتی!", "t.me/{}?start=help".format(BOT_USERNAME))]])
        return
 
     await event.reply(HELP_TEXT, buttons=btn)
