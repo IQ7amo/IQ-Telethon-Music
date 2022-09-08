4d674bbfef8b327f6fb5e6e8155853678b21549f
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-09-09 00:04:25 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "IQmusicxbot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "IQ_Musicbot")
     SUPPORT = os.environ.get("SUPPORT", "IQerenh")
     CHANNEL = os.environ.get("CHANNEL", "xv7amo")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/43380f5c2ed913e8add36.jpg")
