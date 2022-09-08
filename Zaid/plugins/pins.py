e9075d48c989e35e3eec3770f9f50fbd4d2a03f0
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-09-08 17:59:50 +0300

    Update pins.py

--- Zaid/plugins/pins.py
+++ Zaid/plugins/pins.py
@@ -1,8 +1,8 @@
 import os
 
 from telethon import Button, events, types
-from JE313P.status import *
-from JE313P import *
+from Zaid.status import *
+from Zaid import *
 
 
 PINS_TEXT = """
@@ -17,15 +17,15 @@ PINS_TEXT = """
 بۆ پیشاندانی ئەو نامانەی لە گرووپەکەدا هەڵواسراون
 """
 
-@JE313P.on(events.NewMessage(pattern="^[?!/]نامەی پین کراو"))
+@Zaid.on(events.NewMessage(pattern="^[?!/]pin"))
 async def get_pinned(event):
     chat_id = (str(event.chat_id)).replace("-100", "")
 
-    Ok = await JE313P.get_messages(event.chat_id, ids=types.InputMessagePinned()) 
+    Ok = await Zaid.get_messages(event.chat_id, ids=types.InputMessagePinned()) 
     tem = f"نامەی هەڵواسراو لە چاتدا{event.chat.title} ئەو <a href=https://t.me/c/{chat_id}/{Ok.id}>here</a>."
     await event.reply(tem, parse_mode="html", link_preview=False)
 
-@JE313P.on(events.NewMessage(pattern="^[!?/]هەڵواسین ?(.*)"))
+@Zaid.on(events.NewMessage(pattern="^[!?/]unpin ?(.*)"))
 @is_admin
 async def pin(event, perm):
     if not perm.pin_messages:
@@ -37,23 +37,23 @@ async def pin(event, perm):
        return
     input_str = event.pattern_match.group(1)
     if "notify" in input_str:
-       await JE313P.pin_message(event.chat_id, msg, notify=True)
+       await Zaid.pin_message(event.chat_id, msg, notify=True)
        return
-    await JE313P.pin_message(event.chat_id, msg)   
+    await Zaid.pin_message(event.chat_id, msg)   
 
-@JE313P.on(events.NewMessage(pattern="^[!?/]سڕینەوەی هەڵواسین ?(.*)"))
+@Zaid.on(events.NewMessage(pattern="^[!?/]unpin ?(.*)"))
 @is_admin
 async def unpin(event, perm):
     if not perm.pin_messages:
        await event.reply("پێویستە سەرەتا مۆڵەتی هەڵواسینت هەبێت")
        return
     chat_id = (str(event.chat_id)).replace("-100", "")
-    ok = await JE313P.get_messages(event.chat_id, ids=types.InputMessagePinned())
-    await JE313P.unpin_message(event.chat_id, ok)
+    ok = await Zaid.get_messages(event.chat_id, ids=types.InputMessagePinned())
+    await Zaid.unpin_message(event.chat_id, ok)
     await event.reply(f"بە سەرکەوتوویی لابردنی هەڵواسین[بۆ ئەم نامەیە](t.me/{event.chat.username}/{ok.id}).", link_preview=False)
 
 
-@JE313P.on(events.NewMessage(pattern="^[!?/]سڕینەوەی گشتی$"))
+@Zaid.on(events.NewMessage(pattern="^[!?/]unpinall$"))
 async def unpinall(event, perm):
     if not perm.pin_messages:
        await event.reply("پێویستە سەرەتا مۆڵەتی هەڵواسینت هەبێت")
@@ -62,32 +62,32 @@ async def unpinall(event, perm):
 ئایا دڵنیای کە نامەکان هەڵدەگریت ؟
 """
 
-    await JE313P.send_message(event.chat_id, UNPINALL, buttons=[
+    await Zaid.send_message(event.chat_id, UNPINALL, buttons=[
     [Button.inline("دووپاتکردنەوە", data="unpin")], 
     [Button.inline("هەڵوەشاندنەوە", data="cancel")]])
 
-@JE313P.on(events.callbackquery.CallbackQuery(data="unpin"))
+@Zaid.on(events.callbackquery.CallbackQuery(data="unpin"))
 async def confirm(event):
     check = await event.client.get_permissions(event.chat_id, event.sender_id)
     if check.is_creator:
-        await JE313P.unpin_message(event.chat_id)
+        await Zaid.unpin_message(event.chat_id)
         await event.edit("بە سەرکەوتوویی هەموو هەڵواسراوەکان هەڵوەشاندرانەوه")
         return 
 
     await event.answer("پێویستە سەرەتا خاوەنی گروپەکە بیت")
 
-@JE313P.on(events.callbackquery.CallbackQuery(data="cancel"))
+@Zaid.on(events.callbackquery.CallbackQuery(data="cancel"))
 async def cancel(event):
 
     check = await event.client.get_permissions(event.chat_id, event.sender_id)
     if check.is_creator:
-        await event.edit("پرۆسەی لابردنی دامەزراندن بۆ هەموو نامەکان هەڵوەشاوەتەوە ")
+        await event.edit("پرۆسەی لابردنی هەڵواسین بۆ هەموو نامەکان هەڵوەشاوەتەوە ")
         return 
 
     await event.answer("پێویستە سەرەتا خاوەنی گروپەکە بیت")
 
 
-@JE313P.on(events.callbackquery.CallbackQuery(data="pins"))
+@Zaid.on(events.callbackquery.CallbackQuery(data="pins"))
 async def _(event):
 
     await event.edit(PINS_TEXT, buttons=[[Button.inline("گەڕانەوە", data="help")]])
