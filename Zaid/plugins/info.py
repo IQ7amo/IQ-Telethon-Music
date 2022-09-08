f4184ca1cecb75a9fc3a3b5a7b61171a0f2ce5fd
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-09-08 17:40:41 +0300

    Update info.py

--- Zaid/plugins/info.py
+++ Zaid/plugins/info.py
@@ -1,6 +1,6 @@
 from telethon import events, Button, types
-from JE313P import JE313P
-from JE313P.status import *
+from Zaid import Zaid
+from Zaid.status import *
 from telethon.tl.types import ChannelParticipantsAdmins
 from datetime import timedelta
 from telethon.tl.functions.photos import GetUserPhotosRequest as P
@@ -15,7 +15,7 @@ MISC_HELP = """
 بۆ پیشاندانی زانیاری بەکارهێنەر بە وەڵامدانەوەی
 """
 
-@JE313P.on(events.NewMessage(pattern="^[!?/]ID"))
+@Zaid.on(events.NewMessage(pattern="^[!?/]ID"))
 async def id(event):
 
     if event.is_private:
@@ -34,11 +34,11 @@ async def id(event):
 
     await event.reply(f"بەکارهێنەر {msg.sender.first_name} /n ناسنامە `{msg.sender_id}`.")
  
-@JE313P.on(events.NewMessage(pattern="^[!?/]info ?(.*)"))
+@Zaid.on(events.NewMessage(pattern="^[!?/]info ?(.*)"))
 async def info(event):
 
-    sed = await JE313P(P(user_id=event.sender_id, offset=42, max_id=0, limit=80))
-    hn = await JE313P(GetFullUserRequest(event.sender_id))
+    sed = await Zaid(P(user_id=event.sender_id, offset=42, max_id=0, limit=80))
+    hn = await Zaid(GetFullUserRequest(event.sender_id))
     text = "**زانیاری بەکارهێنەر:**\n\n"
     text += "**ناوی یەکەم:** {}\n"
     text += "**ناوی دووەم:** {}\n"
@@ -50,13 +50,13 @@ async def info(event):
 
     input_str = event.pattern_match.group(1)
     if not input_str:
-          await JE313P.send_message(event.chat_id, text.format(hn.user.first_name, hn.user.last_name, event.sender_id, event.sender.username, sed.count, hn.about, event.sender_id))
+          await Zaid.send_message(event.chat_id, text.format(hn.user.first_name, hn.user.last_name, event.sender_id, event.sender.username, sed.count, hn.about, event.sender_id))
           return
  
     input_str = event.pattern_match.group(1)
-    ha = await JE313P.get_entity(input_str)
-    hu = await JE313P(GetFullUserRequest(id=input_str))
-    sedd = await JE313P(P(user_id=input_str, offset=42, max_id=0, limit=80))
+    ha = await Zaid.get_entity(input_str)
+    hu = await Zaid(GetFullUserRequest(id=input_str))
+    sedd = await Zaid(P(user_id=input_str, offset=42, max_id=0, limit=80))
 
     text = "**زانیاری بەکارهێنەر:**\n\n"
     text += "**ناوی یەکەم:** {}\n"
@@ -70,6 +70,6 @@ async def info(event):
     await event.reply(textn.format(ha.first_name, ha.last_name, ha.id, ha.username, sedd.count, hu.about, ha.id))
    
 
-@JE313P.on(events.callbackquery.CallbackQuery(data="misc"))
+@Zaid.on(events.callbackquery.CallbackQuery(data="misc"))
 async def _(event):
     await event.edit(MISC_HELP, buttons=[[Button.inline("گەڕانەوە", data="help")]])
